e9f90570c1d7c07961de72142a1af5d1ccfc0d02
Abdelmejid Oumer Ali <abdelmejid89@gmail.com>
2022-08-31 16:34:20 +0300

    Link sass files

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
 import Cities from './pages/Cities';
 import City from './pages/City';
 import Countries from './pages/Countries';
 import States from './pages/States';
 
 const App = () => (
-  <Routes>
-    <Route path="/" element={<Countries />} />
-    <Route path="/:country" element={<States />} />
-    <Route path="/:country/:state" element={<Cities />} />
-    <Route path="/:country/:state/:city" element={<City />} />
-  </Routes>
+  <>
+    <Navbar />
+    <Routes>
+      <Route path="/" element={<Countries />} />
+      <Route path="/:country" element={<States />} />
+      <Route path="/:country/:state" element={<Cities />} />
+      <Route path="/:country/:state/:city" element={<City />} />
+    </Routes>
+  </>
 );
 
 export default App;
