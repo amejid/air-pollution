8c2f75cb14a528bbbe006cbf97a185452c444463
Abdelmejid Oumer Ali <abdelmejid89@gmail.com>
2022-08-31 12:51:05 +0300

    Setup routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
-const App = () => {
-  return <div className="App">Hello</div>;
-};
+import { Routes, Route } from 'react-router-dom';
+import Cities from './pages/Cities';
+import City from './pages/City';
+import Countries from './pages/Countries';
+import States from './pages/States';
+
+const App = () => (
+  <Routes>
+    <Route path="/" element={<Countries />} />
+    <Route path="/:country/states" element={<States />} />
+    <Route path="/:country/:state/cities" element={<Cities />} />
+    <Route path="/:country/:state/:city" element={<City />} />
+  </Routes>
+);
 
 export default App;
