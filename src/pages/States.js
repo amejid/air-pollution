92bbb484dab27d805ee2b69374d6963b46c2d07c
Abdelmejid Oumer Ali <abdelmejid89@gmail.com>
2022-09-01 16:23:02 +0300

    Update link

--- src/pages/States.js
+++ src/pages/States.js
@@ -1,4 +1,4 @@
-import { Link, useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
 import { getStates } from '../redux/states/states';
@@ -6,7 +6,6 @@ import { getStates } from '../redux/states/states';
 let initial = true;
 
 const States = () => {
-  const navigate = useNavigate();
   const states = useSelector((state) => state.states);
   const dispatch = useDispatch();
 
@@ -18,22 +17,15 @@ const States = () => {
   }, [dispatch]);
 
   return (
-    <>
-      <div className="btn-container">
-        <button className="btn" type="button" onClick={() => navigate(-1)}>
-          Back
-        </button>
-      </div>
-      <ul className="cards">
-        {states.map((state) => (
-          <li className="cards__item" key={state}>
-            <Link className="cards__link" to={`/${state}`}>
-              {state}
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </>
+    <ul className="cards">
+      {states.map((state) => (
+        <li className="cards__item" key={state}>
+          <Link className="cards__link" to={`/${state}`}>
+            {state}
+          </Link>
+        </li>
+      ))}
+    </ul>
   );
 };
 
