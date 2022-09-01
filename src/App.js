4793a26b9750de2542faeec617e5ea0595b6e3d5
Abdelmejid Oumer Ali <abdelmejid89@gmail.com>
2022-09-01 13:18:13 +0300

    Update routes

--- src/App.js
+++ src/App.js
@@ -2,17 +2,15 @@ import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Cities from './pages/Cities';
 import City from './pages/City';
-import Countries from './pages/Countries';
 import States from './pages/States';
 
 const App = () => (
   <>
     <Navbar />
     <Routes>
-      <Route path="/" element={<Countries />} />
-      <Route path="/:country" element={<States />} />
-      <Route path="/:country/:state" element={<Cities />} />
-      <Route path="/:country/:state/:city" element={<City />} />
+      <Route path="/" element={<States />} />
+      <Route path="/:state" element={<Cities />} />
+      <Route path="/:state/:city" element={<City />} />
     </Routes>
   </>
 );
